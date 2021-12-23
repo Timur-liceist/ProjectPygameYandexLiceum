9dd68421c6d3b8d33e419050f4a9d41619ba5e3b
timka <ochilov-timka@yandex.ru>
2021-12-23 18:30:38 +0300

    Добавили спрайт танчика

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 all_sprites = pygame.sprite.Group()
-
+cell_size = 20
 
 def load_image(name, w, h, colorkey=None):
     fullname = os.path.join('data', name)
@@ -33,7 +33,7 @@ def terminate():
 
 
 class Block(pygame.sprite.Sprite):
-    image_of_NotBreak = load_image("ntbreak_blok.png", 20, 20)
+    image_of_NotBreak = load_image("ntbreak_blok.png", cell_size, cell_size)
 
     def __init__(self, coords, type):
         super().__init__(all_sprites)
@@ -49,7 +49,7 @@ class Block(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
 
-    def update(self):
+    def update(self, *args):
         pass
 
 
@@ -64,7 +64,7 @@ class Grass(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-    def update(self):
+    def update(self, *args):
         pass
 
 
@@ -84,7 +84,15 @@ class Board:
         # значения по умолчанию
         self.left = 0
         self.top = 0
-        self.cell_size = 20
+        self.cell_size = cell_size
+        for w in range(self.width):
+            for h in range(self.height):
+                x = self.left + w * self.cell_size
+                y = self.top + h * self.cell_size
+                if self.board[h][w] == 0:
+                    Grass(w * cell_size, h * cell_size)
+                if self.board[h][w] == 1:
+                    Block((w * cell_size, h * cell_size), 1)
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -93,17 +101,60 @@ class Board:
         self.cell_size = cell_size
 
     def render(self, screen):
-        for w in range(self.width):
-            for h in range(self.height):
-                x = self.left + w * self.cell_size
-                y = self.top + h * self.cell_size
-                if self.board[h][w] == 0:
-                    Grass(w * 20, h * 20)
-                if self.board[h][w] == 1:
-                    Block((w * 20, h * 20), 1)
+        # for w in range(self.width):
+        #     for h in range(self.height):
+        #         x = self.left + w * self.cell_size
+        #         y = self.top + h * self.cell_size
+        #         if self.board[h][w] == 0:
+        #             Grass(w * cell_size, h * cell_size)
+        #         if self.board[h][w] == 1:
+        #             Block((w * cell_size, h * cell_size), 1)
+        pass
+class Tank(pygame.sprite.Sprite):
+    image_of_tank = load_image("tank.png", 20, 20)
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        self.image = Tank.image_of_tank
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.x, self.y = x, y
 
-                pygame.draw.rect(screen, "white", (x, y, self.cell_size, self.cell_size), 1)
+    # def shoot(self, x, y, vx, vy):
+    #     pass
+    def move(self, x, y):
+        tx = x
+        ty = y
+        if x != self.x and y != self.y:
+            return
+        if x > self.x:
+            self.image = pygame.transform.rotate(Tank.image_of_tank, 270)
+        if x < self.x:
+            self.image = pygame.transform.rotate(Tank.image_of_tank, 90)
+        if y > self.y:
+            self.image = pygame.transform.rotate(Tank.image_of_tank, 180)
+        if y < self.y:
+            self.image = Tank.image_of_tank
+        self.rect.x = x
+        self.rect.y = y
+        self.x = x
+        self.y = y
 
+    def update(self, *args):
+        # # if pygame.KEYDOWN == args[0].type:
+        # #     print(2)
+        # #     if args[0].key == pygame.K_w:
+        # #         self.rect.y += 10
+        # if pygame.KEYDOWN == args[0].type:
+        #     if args[0].key == pygame.K_w:
+        #         self.rect.y += 10
+        # if pygame.KEYUP == args[0].type:
+        #     if args[0].key == pygame.K_w:
+        #         # self.rect.y += 10
+        #          pass
+        pass
+    # def update(self, *args):
+    #     if args[0].type ==
 
 pygame.display.set_caption('Танчики')
 screen = pygame.display.set_mode((500, 500))
@@ -115,6 +166,10 @@ clock = pygame.time.Clock()
 # for j in range(25):
 # Grass(i * 20, j * 20)
 running = True
+go_tank1 = False
+dx_tank1 = 0
+dy_tank1 = 0
+tank1 = Tank(60, 60)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -122,11 +177,44 @@ while running:
         if event.type == pygame.KEYDOWN:
             # player1
             if event.key == pygame.K_w: # Обработка клавици "w"
-                pass
-
+                dy_tank1 = -0.5
+                print("w_down")
+                go_tank1 = True
+            if event.key == pygame.K_s: # Обработка клавици "w"
+                dy_tank1 = 0.5
+                print("s_down")
+                go_tank1 = True
+            if event.key == pygame.K_a: # Обработка клавици "w"
+                dx_tank1 = -0.5
+                print("a_down")
+                go_tank1 = True
+            if event.key == pygame.K_d: # Обработка клавици "w"
+                dx_tank1 = 0.5
+                print("d_down")
+                go_tank1 = True
+        if event.type == pygame.KEYUP:
+            # player1
+            if event.key == pygame.K_w: # Обработка клавици "w"
+                dy_tank1 = 0
+                print("w_up")
+                go_tank1 = False
+            if event.key == pygame.K_s: # Обработка клавици "w"
+                dy_tank1 = 0
+                print("s_up")
+                go_tank1 = False
+            if event.key == pygame.K_a: # Обработка клавици "w"
+                dx_tank1 = 0
+                print("a_up")
+                go_tank1 = False
+            if event.key == pygame.K_d: # Обработка клавици "w"
+                dx_tank1 = 0
+                print("d_up")
+                go_tank1 = False
+    if go_tank1:
+        tank1.move(tank1.x + dx_tank1, tank1.y + dy_tank1)
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
-    all_sprites.update()
+    all_sprites.update(event)
     playground.render(screen)
     pygame.display.flip()
     clock.tick(fps)
